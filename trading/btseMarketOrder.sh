54eebb7a097a237f87d3374595c38c412de5c985
castiello.tsai <castiello.tsai@nogle.com>
2024-05-04 23:39:46 +0800

    add close position

--- trading/btseMarketOrder.sh
+++ trading/btseMarketOrder.sh
@@ -2,17 +2,18 @@
 # brew install coreutils jsonpp
 ## Options
 # $(uuidgen | tr 'A-Z' 'a-z')
-function buy {
-curl 'https://api.btse.com/futures/api/v2.1/order' \
-  -H @./authorization.clrc \
-  --data-raw '{"side":"BUY","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$2'"}' \
-  | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
+function placeMarketOrder {
+  curl 'https://api.btse.com/futures/api/v2.1/order' \
+    -H @./authorization.clrc \
+    --data-raw '{"side":"'$1'","symbol":"BTC-PERP","type":"MARKET","size":"'$2'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$(uuidgen | tr 'A-Z' 'a-z')'"}' \
+    | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
 }
-function sell {
-curl 'https://api.btse.com/futures/api/v2.1/order' \
-  -H @./authorization.clrc \
-  --data-raw '{"side":"SELL","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$2'"}' \
-  | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
+function closePosition {
+  curl 'https://api.btse.com/futures/api/v2.1/order/close_position' \
+    -H @./authorization.clrc \
+    --data-raw '{"symbol":"BTC-PERP","type":"MARKET","clOrderID":"_W_'$(uuidgen | tr 'A-Z' 'a-z')'"}' \
+    | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
+
 }
 
 case $1 in
@@ -20,29 +21,32 @@ case $1 in
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      buy $2
+      placeMarketOrder 'BUY' $2 &
     done
     ;;
   SELL|sell|S|s):
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      sell $2 
+      placeMarketOrder 'SELL' $2 &
     done
     ;;
   rb):
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      buy $(($RANDOM % $2 + 1)) $(uuidgen | tr 'A-Z' 'a-z') &
+      placeMarketOrder 'BUY' $(($RANDOM % $2 + 1)) &
     done
     ;;
   rs):
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      sell $(($RANDOM % $2 + 1)) $(uuidgen | tr 'A-Z' 'a-z') &
+      placeMarketOrder 'SELL' $(($RANDOM % $2 + 1)) &
     done
+    ;;
+  cp):
+    closePosition
     ;;
   *)
     echo 'What are you tolking about'
