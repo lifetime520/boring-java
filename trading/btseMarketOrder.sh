f0d8c69d286d4b1f2857e68d80913e81585f97ea
castiello.tsai <castiello.tsai@nogle.com>
2024-05-04 22:38:40 +0800

    update field

--- trading/btseMarketOrder.sh
+++ trading/btseMarketOrder.sh
@@ -1,14 +1,18 @@
+## README
+# brew install coreutils jsonpp
+## Options
+# $(uuidgen | tr 'A-Z' 'a-z')
 function buy {
 curl 'https://api.btse.com/futures/api/v2.1/order' \
   -H @./authorization.clrc \
-  --data-raw '{"side":"BUY","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY"}' \
-  | json_pp >> ~/trading/logs/`date +"%Y%m%d"`.btse.log &
+  --data-raw '{"side":"BUY","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$2'"}' \
+  | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
 }
 function sell {
 curl 'https://api.btse.com/futures/api/v2.1/order' \
   -H @./authorization.clrc \
-  --data-raw '{"side":"SELL","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY"}' \
-  | json_pp >> ~/trading/logs/`date +"%Y%m%d"`.btse.log &
+  --data-raw '{"side":"SELL","symbol":"BTC-PERP","type":"MARKET","size":"'$1'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$2'"}' \
+  | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
 }
 
 case $1 in
@@ -26,18 +30,18 @@ case $1 in
       sell $2 
     done
     ;;
-  rndBUY|rndBuy|rB|rb):
+  rb):
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      buy $(($RANDOM % $2 + 1))
+      buy $(($RANDOM % $2 + 1)) $(uuidgen | tr 'A-Z' 'a-z') &
     done
     ;;
-  rndSELL|rndSell|rS|rs):
+  rs):
     for i in `seq 1 $3`;
     do
       echo 'Round:' $i
-      sell $(($RANDOM % $2 + 1))
+      sell $(($RANDOM % $2 + 1)) $(uuidgen | tr 'A-Z' 'a-z') &
     done
     ;;
   *)
