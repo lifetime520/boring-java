3ca9461941f0dea7c1cae0562465e33e200d1262
castiello.tsai <castiello.tsai@nogle.com>
2024-05-05 23:07:25 +0800

    close position api not ready

--- trading/btseMarketOrder.sh
+++ trading/btseMarketOrder.sh
@@ -6,14 +6,18 @@ function placeMarketOrder {
   curl 'https://api.btse.com/futures/api/v2.1/order' \
     -H @./authorization.clrc \
     --data-raw '{"side":"'$1'","symbol":"BTC-PERP","type":"MARKET","size":"'$2'","reduceOnly":false,"txType":"LIMIT","positionMode":"ONE_WAY","clOrderID":"_W_'$(uuidgen | tr 'A-Z' 'a-z')'"}' \
-    | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
+    | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(timestamp" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
 }
 function closePosition {
-  curl 'https://api.btse.com/futures/api/v2.1/order/close_position' \
-    -H @./authorization.clrc \
-    --data-raw '{"symbol":"BTC-PERP","type":"MARKET","clOrderID":"_W_'$(uuidgen | tr 'A-Z' 'a-z')'"}' \
-    | json_pp | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(size" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
-
+  curl 'https://api.btse.com/futures/api/v2.2/order/close_position' \
+    -H 'accept: application/json, text/plain, */*' \
+    -H 'authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTQ4NjY4OTcsImV4cCI6MTcxNDk1MzM1NywiaXNzIjoiYnRzZS5wcmQiLCJhdWQiOiJjbGllbnQuc2VydmljZSIsInVzZXJuYW1lIjoiY2FzdGllbGxvdHNhaSIsImNsaWVudElkIjoiYnRzZSIsInJhbmRvbSI6IjAuNzg0NTI4MDgyNDYzODYyMSIsImRldmljZVR5cGUiOiJ3ZWIiLCJkZXZpY2VJZCI6bnVsbCwic2NvcGVzIjpbInRydXN0ZWQiXX0.TYubZ6HEl7wwgIGYmePXE4modMZB61tAAe5b0sYk2fw' \
+    -H 'content-type: application/json' \
+    -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
+    --data-raw '{"symbol":"BTC-PERP","type":"MARKET","positionId":"BTC-PERP-USDT","clOrderID":"_W_'$(uuidgen | tr 'A-Z' 'a-z')'"}' 
+    # \
+    # | json_pp
+     # | grep -v -E 'ion(D|M)|dev|order(D|T)|time_i|tr|sym|Si|lth|On' | sed -E 's/(timestamp" : [0-9]+),/\1/g' >> ./logs/`date +"%Y%m%d"`.btse.log &
 }
 
 case $1 in
